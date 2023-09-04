8c6656fc21d4a177437a6d60eaea0b42a17fc267
imran2205 <imrankabir1996@gmail.com>
2023-09-04 15:51:05 -0400

    fixed frame (0) selection not synced with heatmap

--- random_csv_generator.py
+++ random_csv_generator.py
@@ -2,8 +2,8 @@ import os
 import pandas as pd
 import random
 
-input_dir = 'C:/Users/Touhid Shohan/Downloads/GT/Ground_Truth_Annotation'
-output_dir = 'C:/Users/Touhid Shohan/Downloads/Random'
+input_dir = '/Users/imrankabir/Downloads/nbn/gt'
+output_dir = '/Users/imrankabir/Downloads/nbn/random'
 predefined_list_file = 'all_a11y_objects.txt'
 
 random.seed(42)
@@ -11,14 +11,14 @@ random.seed(42)
 with open(predefined_list_file, 'r') as file:
     predefined_list = [item.strip() for item in file.read().split(',')]
 
-xlsx_files = [f for f in os.listdir(input_dir) if f.endswith('.xlsx')]
+xlsx_files = [f for f in os.listdir(input_dir) if f.endswith('.csv')]
 
 for xlsx_file in xlsx_files:
     xlsx_path = os.path.join(input_dir, xlsx_file)
     csv_file = os.path.splitext(xlsx_file)[0].replace('_', '-') + '.csv'
     csv_path = os.path.join(output_dir, csv_file)
 
-    df = pd.read_excel(xlsx_path)
+    df = pd.read_csv(xlsx_path)
 
     new_column_names = [col.replace('frame_', 'Frame-') if col.startswith('frame_') else col for col in df.columns]
     df.columns = new_column_names
