58f9ad69567617af8cdd18dfecae7f4d9ec93a36
imran2205 <imrankabir1996@gmail.com>
2024-02-19 17:36:05 -0500

    object dropdown color change

---
+++ assets/custom_function.js
@@ -0,0 +1,52 @@
+
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     setTimeout(function () {
+//         var dropdown = document.querySelectorAll('#I-see .');
+//
+//         console.log(dropdown);
+//         dropdown.addEventListener('change', function() {
+//             console.log("sss");
+//             var spans = document.querySelectorAll('#I-see .Select-value-label');
+//             alert(spans);
+//             // Apply red color to all selected options
+//             // Array.from(dropdown.selectedOptions).forEach(function(option) {
+//             //     alert(option.value);
+//             //     option.style.color = 'red';
+//             // });
+//         });
+//
+//     }, 2000);
+//
+// });
+
+// function change_color(){
+//     var dropdown = document.querySelectorAll('#I-see .');
+//
+//     console.log(dropdown);
+//     dropdown.addEventListener('change', function() {
+//         console.log("sss");
+//         var spans = document.querySelectorAll('#I-see .Select-value-label');
+//         alert(spans);
+//         // Apply red color to all selected options
+//         // Array.from(dropdown.selectedOptions).forEach(function(option) {
+//         //     alert(option.value);
+//         //     option.style.color = 'red';
+//         // });
+//     });
+// }
+
+window.dash_clientside = Object.assign({}, window.dash_clientside, {
+    myNamespace: {
+        customFunction: function() {
+            // Your custom JavaScript code here
+            // console.log('Custom function called with argument: ');
+            // return 'Result from customFunction: ';
+            var spans = document.querySelectorAll('#I-see .Select-value-label');
+            for (var i = 0; i < spans.length; i++) {
+                console.log(spans[i].value)
+                spans[i].style.color = 'red';
+            }
+        }
+    }
+});
