7cb518a5aa450cf65fd9fd37cdfadd25e59721e4
imran2205 <imrankabir1996@gmail.com>
2024-03-30 15:30:32 -0400

    latex with matplotlib

---
+++ log_analysis/final_original_mpl.py
@@ -0,0 +1,173 @@
+# import necessary libraries
+import plotly.graph_objects as go
+import time
+import numpy as np
+import plotly
+import pandas as pd
+import statistics
+from IPython.display import display, HTML
+import scipy.stats
+from sklearn.linear_model import LinearRegression
+
+plotly.offline.init_notebook_mode()
+display(HTML(
+    '<script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.0.0/es5/latest?tex-mml-chtml.js"></script>'
+))
+
+# Define the F1 scores
+F1 = {
+    'Ground Truth': 1.0,
+    'GPT4V': 0.899,
+    'GPT4V@Shadow': 0.899,
+    'BLIP': 0.822,
+    'BLIP@Shadow': 0.822,
+    'GPV-1': 0.786,
+    'GPV-1@Shadow': 0.786,
+    'Random': 0.334,
+}
+
+# Load the combined CSV
+df = pd.read_csv('../Logs/trimmed_logs/all.csv')
+
+# Convert 'model left' column to string
+df['model left'] = df['model left'].astype(str)
+
+# Exclude participants P2, P6, P9, and P14
+# exclude_videos = ['video-6-segment-2', 'video-7-segment-2', 'video-13-segment-2', 'video-1-segment-5']
+# df = df[~df['video'].isin(exclude_videos)]
+
+# Define the desired order and ensure it matches F1 dictionary
+desired_order = ['Random', 'GPV-1', 'BLIP', 'GPT4V', 'Ground Truth']
+
+# Filter dataframe based on desired order
+df = df[df['model left'].isin(desired_order)]
+
+# Adjustments for thinner boxes and increased spacing
+box_width = 0.35  # Decrease box width for thinner boxes
+
+# Generate ticktext with model names and F1 scores
+f_mathcal_d = r"F_{1}^{\mathcal{D}}"
+ticktext = [r"$\text{" + f"{model}" + r"}" + r" \\ (" + f_mathcal_d + f": {F1[model]:.3f})" + r"$" for model in
+            desired_order]
+
+fig = go.Figure()
+
+colors = {
+    'Random': 'red',
+    'Ground Truth': 'green',
+    'GPV-1': '#6012cc',
+    'BLIP': '#0dcfd6',
+    'GPT4V': '#0a63f2'
+}
+
+# Iterate over each model in the desired order
+for model in desired_order:
+    # Determine color based on the presence of 'Shadow'
+    color = colors[model]
+
+    # Extract scores for the current model
+    scores = df[df['model left'] == model]['normalized_score']
+    s_med = statistics.median(scores)
+
+    # Add box plot trace for the current model with adjusted box width
+    fig.add_trace(go.Box(
+        y=scores,
+        name=f"{model} ({F1[model]:.3f})",  # Include F1 score in the name for legend
+        marker=dict(
+            color=color,  # Color for the box outline and whiskers
+        ),
+        line=dict(
+            color=color,  # Ensure this matches marker color for consistency
+            width=2  # Set line width for box
+        ),
+        fillcolor='white',  # Set the fill color of the box to white
+        boxpoints=False,  # Do not show all data points
+        showlegend=False,
+        width=box_width  # Adjust box width here
+    ))
+
+# Adjust the figure's layout
+fig.update_layout(
+    title=dict(
+        text='User Ratings for Different Models',
+        x=0.5,  # Center align the title
+        font=dict(family='Arial', size=20, color='black'),  # Increased font size
+    ),
+    yaxis_title='User Rating',
+    xaxis=dict(
+        title='Model',
+        ticktext=ticktext,
+        tickvals=list(range(len(desired_order))),  # Position each tick
+    ),
+    plot_bgcolor='white',
+    font=dict(family='Arial', size=14),  # Increased font size
+    yaxis=dict(
+        showgrid=True,  # Show horizontal gridlines
+        gridwidth=0.5,
+        gridcolor='lightgrey',
+        dtick=0.1,  # Set gridline interval
+        range=[0, 1.25],
+        tickvals=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0],
+        ticktext=['0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1', '', '']
+        # Empty strings for labels at 1.1 and 1.2
+    ),
+    width=600,  # Adjusted width
+    height=700
+)
+
+fig.update_xaxes(tickangle=45)
+
+
+# Function to add statistical significance annotations
+def add_stat_signf(x0, x1, y, signf_level, fig):
+    # Add a horizontal line from x=0 to x=1 at y=0.95
+    fig.add_shape(type="line",
+                  x0=x0, y0=y, x1=x1, y1=y,
+                  line=dict(color="black", width=1))
+
+    # Adding caps to the horizontal line to create tips at both ends
+    fig.add_shape(type="line",
+                  x0=x0, y0=y - 0.005, x1=x0, y1=y + 0.005,
+                  line=dict(color="black", width=1))
+
+    fig.add_shape(type="line",
+                  x0=x1, y0=y - 0.005, x1=x1, y1=y + 0.005,
+                  line=dict(color="black", width=1))
+
+    # Add annotation at the center of the line
+    if signf_level != 'NS':
+        fig.add_annotation(x=(x0 + x1) / 2, y=y, text=signf_level,
+                           showarrow=False, font=dict(family="Arial", size=16))
+    else:
+        fig.add_annotation(x=(x0 + x1) / 2, y=y + 0.02, text=signf_level,
+                           showarrow=False, font=dict(family="Arial", size=16))
+
+    # Add statistical significance annotations
+
+
+add_stat_signf(0, 4, 1.2, '***', fig=fig)
+
+add_stat_signf(0, 3, 1.175, '***', fig=fig)
+add_stat_signf(1, 4, 1.15, '***', fig=fig)
+
+add_stat_signf(0, 2, 1.125, '***', fig=fig)
+add_stat_signf(1, 3, 1.1, '**', fig=fig)
+add_stat_signf(2, 4, 1.075, '***', fig=fig)
+
+add_stat_signf(0, 1, 1.05, '***', fig=fig)
+add_stat_signf(3, 4, 1.025, '***', fig=fig)
+
+add_stat_signf(1, 2, 0.95, 'NS', fig=fig)
+add_stat_signf(2, 3, 0.975, 'NS', fig=fig)
+
+# Add R^2 annotation
+fig.add_annotation(x=4.4, y=0.85, text=r"$R^{2} = 0.9$",
+                   showarrow=False, font=dict(family="Arial", size=18),
+                   textangle=90)
+
+fig.show()
+
+# Save the figure as a PDF
+fig.write_image('../paper_files_latex/' + 'all_org_model_scores.pdf', format='pdf')
+time.sleep(1.5)  # Ensure the file is saved before attempting to save again
+fig.write_image('../paper_files_latex/' + 'all_org_model_scores.pdf', format='pdf')
